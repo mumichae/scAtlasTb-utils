d4a0f60d3f42ad303ffbce6863ca82ffb8590371
ktpolanski <krzysztof_polanski@o2.pl>
2025-08-14 13:29:37 +0000

    unify param syntax and split into two tests for legibility

--- tests/test_library_obs.py
+++ tests/test_library_obs.py
@@ -4,6 +4,9 @@ import pytest
 import scatlastb_utils as atl
 
 
+# this defines a simple dummy obs data frame for testing
+# can use obs as a positional argument for tests and it will pull in this
+# (that's what the fixture part is for)
 @pytest.fixture
 def obs():
     return pd.DataFrame.from_dict(
@@ -18,6 +21,9 @@ def obs():
     )
 
 
+# this is the syntax for testing a bunch of different combinations
+# need to provide the argument names, matching the actual test function below
+# and then a variety of inputs
 @pytest.mark.parametrize(
     "barcodes,expected,raises",
     [
@@ -37,56 +43,61 @@ def obs():
 )
 def test_strip_barcodes(barcodes, expected, raises):
     """Test atl.pp.strip_barcodes for valid and invalid inputs."""
+    # the syntax for checking for an exception and checking the actual output are different
+    # so if the expected exception is not None, go in and handle it exception style
     if raises:
         with pytest.raises(raises):
             atl.pp.strip_barcodes(barcodes)
     else:
+        # and if it is none, check an assert of the output
         assert atl.pp.strip_barcodes(barcodes) == expected
 
 
+# in the interest of legibility, the actual tests for this function are split into two
+# in this part, we're checking for exceptions
 @pytest.mark.parametrize(
-    "barcodes,library_key,expected_exception,expected_shape,expected_found",
+    "barcodes,library_key,expected_exception",
     [
         # KeyError: default library key missing
-        (
+        pytest.param(
             ["ACTGACTGACTGAAAA-different-ending", "PREFIX_ACTGACTGACTGTTTT", "missing-ACTGACTGACTGGGGG-from-pool"],
             None,
             KeyError,
-            None,
-            None,
+            id="missing default library key (good barcodes)",
         ),
-        # KeyError: default library key missing
-        (["this-will-brick"], None, KeyError, None, None),
+        # KeyError: default library key missing (should be checked before the barcodes)
+        pytest.param(["this-will-brick"], None, KeyError, id="missing default library key (bad barcodes)",),
         # ValueError: bricked barcodes
-        (["this-will-brick"], "library", ValueError, None, None),
-        # Success case
-        (
-            ["ACTGACTGACTGAAAA-different-ending", "PREFIX_ACTGACTGACTGTTTT", "missing-ACTGACTGACTGGGGG-from-pool"],
-            "library",
-            None,
-            (3, 2),
-            {"library": "A1", "input_cell_count": 3, "library_cell_count": 3, "overlap": 2},
-        ),
-    ],
-    ids=[
-        "missing default library key (bc1)",
-        "missing default library key (bc2)",
-        "invalid barcode with library key",
-        "valid input with library key",
+        pytest.param(["this-will-brick"], "library", ValueError, id="invalid barcode with library key",),
     ],
 )
-def test_find_library_obs(barcodes, library_key, expected_exception, expected_shape, expected_found, obs):
-    if expected_exception:
-        if library_key:
-            with pytest.raises(expected_exception):
-                atl.pp.find_library_obs(barcodes, obs, library_key=library_key)
-        else:
-            with pytest.raises(expected_exception):
-                atl.pp.find_library_obs(barcodes, obs)
+def test_find_library_obs_exceptions(barcodes, library_key, expected_exception, obs):
+    if library_key:
+        with pytest.raises(expected_exception):
+            atl.pp.find_library_obs(barcodes, obs, library_key=library_key)
     else:
-        sub, found = atl.pp.find_library_obs(barcodes, obs, library_key=library_key)
-        assert isinstance(sub, pd.DataFrame)
-        assert isinstance(found, pd.Series)
-        assert sub.shape == expected_shape
-        for k, v in expected_found.items():
-            assert found[k] == v
+        with pytest.raises(expected_exception):
+            atl.pp.find_library_obs(barcodes, obs)
+
+
+# here we're checking everything runs as expected
+@pytest.mark.parametrize(
+    "barcodes,library_key,expected_shape,expected_found",
+    [
+        # Success case
+        pytest.param(
+            ["ACTGACTGACTGAAAA-different-ending", "PREFIX_ACTGACTGACTGTTTT", "missing-ACTGACTGACTGGGGG-from-pool"],
+            "library",
+            (3, 2),
+            {"library": "A1", "input_cell_count": 3, "library_cell_count": 3, "overlap": 2},
+            id="valid input with library key",
+        ),
+    ],
+)
+def test_find_library_obs(barcodes, library_key, expected_shape, expected_found, obs):
+    sub, found = atl.pp.find_library_obs(barcodes, obs, library_key=library_key)
+    assert isinstance(sub, pd.DataFrame)
+    assert isinstance(found, pd.Series)
+    assert sub.shape == expected_shape
+    for k, v in expected_found.items():
+        assert found[k] == v
