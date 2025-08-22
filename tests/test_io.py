04235e7efde2c7924bdf60660f1a95da3c2cbc1b
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2025-08-22 14:12:52 +0200

    add test for selecting slots

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,4 +1,5 @@
 import pytest
+import scanpy as sc
 
 import scatlastb_utils as sa
 
@@ -40,6 +41,48 @@ def test_read_anndata(adata, file_type, slots, dask):
             dask=dask,
             backed=dask,
         )
+
+
+@pytest.mark.parametrize("select_keys", [".*--select$", ["0", "1", "2"]])
+def test_read_select(adata, select_keys):
+    import re
+    import tempfile
+    from pathlib import Path
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        adata.uns = {str(i): i for i in range(10)}
+        adata.uns |= {f"{i}--select": i for i in range(10)}
+        sc.pp.pca(adata)
+        adata.obsm["X_pca--select"] = adata.obsm["X_pca"]
+        print(adata)
+
+        # determine keys that should be in new object
+        def check_key_match(select_instruction, key):
+            if isinstance(select_instruction, str):
+                print(key, re.match(select_instruction, key))
+                return re.match(select_instruction, key)
+            elif isinstance(select_instruction, list):
+                return key in select_instruction
+            else:
+                raise ValueError(f"Invalid {select_instruction=!s}")
+
+        key_map = {
+            slot: [key for key in adata.__getattribute__(slot).keys() if check_key_match(select_keys, key)]
+            for slot in ["layers", "uns", "obsm", "obsp"]
+        }
+
+        adata_path = Path(temp_dir) / "test_adata.zarr"
+        adata.write_zarr(adata_path)
+
+        adata = sa.io.read_anndata(
+            adata_path,
+            dask=True,
+            backed=True,
+            select_keys=select_keys,
+        )
+        for slot, keys in key_map.items():
+            slot = adata.__getattribute__(slot)
+            assert sorted(keys) == sorted(slot.keys())
 
 
 @pytest.mark.parametrize(
