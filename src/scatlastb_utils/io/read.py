f39879fbb58cc7527fe13f940db32b0eba3bad6b
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-09-17 15:06:49 +0200

    allow for anndata 0.10

--- src/scatlastb_utils/io/read.py
+++ src/scatlastb_utils/io/read.py
@@ -8,7 +8,11 @@ from typing import Any
 import anndata as ad
 import h5py
 import numpy as np
-from anndata.io import read_elem, sparse_dataset
+
+try:
+    from anndata.experimental import read_elem, sparse_dataset  # anndata >=0.10
+except ModuleNotFoundError:
+    from anndata.io import read_elem, sparse_dataset  # anndata <0.10
 from scipy.sparse import csr_matrix
 from tqdm import tqdm
 
