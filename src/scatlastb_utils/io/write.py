fc27d7319a0fd4f1a5cc6653dcb0afad9210df62
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-11-23 11:39:21 -0800

    remove redundant subset_mask call; add additional assertions and generalise input file type

--- src/scatlastb_utils/io/write.py
+++ src/scatlastb_utils/io/write.py
@@ -209,9 +209,8 @@ def link_zarr(
 
     for slot in ALL_SLOTS:
         if slot in slots_to_link or any(x.startswith(f"{slot}/") for x in slots_to_link):
-            set_mask_per_slot(slot=slot, mask=subset_mask, out_dir=out_dir)
-        else:
-            set_mask_per_slot(slot=slot, mask=None, out_dir=out_dir)
+            continue
+        set_mask_per_slot(slot=slot, mask=None, out_dir=out_dir)
 
     for out_slot, in_slot in slot_map:
         if (
@@ -267,17 +266,27 @@ def write_zarr_linked(
     compute
         Whether to compute dask arrays before writing. Default is False.
     """
-    if in_dir is None:
-        in_dirs = []
-    else:
+    if subset_mask is not None:
+        assert adata.shape[0] == subset_mask[0].sum(), (
+            "Number of observations in adata does not match the provided subset mask."
+        )
+        assert adata.shape[1] == subset_mask[1].sum(), (
+            "Number of variables in adata does not match the provided subset mask."
+        )
+
+    if in_dir:
         in_dir = Path(in_dir)
-        if in_dir.suffix != ".zarr" and not in_dir.name.endswith(".zarr/raw"):
+
+        if in_dir.is_dir():
+            in_dirs = [f.name for f in in_dir.iterdir()]
+        else:  # cannot symlink to non-directory (e.g. h5ad)
             print_flushed(
                 f"Warning: `{in_dir=!r}` is not a top-level zarr directory, not linking any files", verbose=True
             )
             write_zarr(adata, out_dir, compute=compute)
-            return  # exit when in_dir is not a top-level zarr directory
-        in_dirs = [f.name for f in in_dir.iterdir()]
+            return  # exit since no linking can be done
+    else:
+        in_dirs = []
 
     if files_to_keep is None:
         files_to_keep = []
